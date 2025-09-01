f23db6adfe0d7722383bb8e182060687892c7b06
Vansh Baldha <baldhavansh2505@gmail.com>
2025-09-01 13:25:21 +0530

    changes

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/profile", require("./routes/profileRoutes"));
 app.use("/api/feedback", require("./routes/feedbackRoutes"));
 app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/payment", require("./routes/paymetRoutes"));
 
 app.use(cookieParser());
 app.use(errorMiddleware);
